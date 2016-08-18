8710ed9e5c74bfe3e11323a7ba334469757fc566
Paul Hawk <hawkpaul@gmail.com>
2016-08-17 21:38:05 -0500

    refactored app to a mean stack

---
+++ config/express.ts
@@ -0,0 +1,35 @@
+import {config} from './config';
+import path = require('path');
+var  express = require('express');
+
+//middleware
+import morgan       = require('morgan');
+import bodyParser   = require('body-parser');
+import compression  = require('compression');
+
+//routes 
+import routes        = require('../app/routes/main.rt');
+
+
+/// add db config to expres 
+var app = express(); 
+
+// middleware ////////////////////////////////////////////////////////////
+app.use(morgan('dev'));    // logger
+app.use(compression());                                                 // comperess data 
+app.use(bodyParser.urlencoded({ extended: true }));                     // sets file return to right Type 
+app.use(bodyParser.json());                                             // json middleware 
+                                                                        // server favicon
+
+// routes ////////////////////////////////////////////////////////////
+app.use('/', routes);
+
+console.log(path.resolve(__dirname,'..', './client'));
+console.log(path.resolve(__dirname, '..', './node_modules'));
+///////// static folders ///////////////////////////////////////////
+//app.use('/public',       express.static(path.resolve(__dirname, '../..',    'public')));
+app.use(                 express.static(path.resolve(__dirname, '..', './client')));
+app.use('/node_modules', express.static(path.resolve(__dirname, '..', './node_modules')));
+
+///////// export configured express ///////////////////////////////
+export = app; 
